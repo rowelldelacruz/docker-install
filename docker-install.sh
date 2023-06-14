fb3cfd1984983aacf2f31199ebae6296bdc91741
rowelldelacruz <39016567+rowelldelacruz@users.noreply.github.com>
2023-06-14 17:08:18 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release openssh-server
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release openssh-server qemu-guest-agent
+sudo systemctl enable qemu-guest-agent
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
